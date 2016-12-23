9248d1ee2fbe3abdaaa8d6c439fb7adf73a11f66
seochangwook <scw05313315@gmail.com>
2016-12-23 13:28:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/apple/kotlin_sample/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.apple.kotlin_sample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.androidhuman.example.hellokotlin.HelloKt;
+
+public class MainActivity extends AppCompatActivity {
+    TextView sample_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        sample_text = (TextView)findViewById(R.id.sample_textview);
+
+        sample_text.setText(HelloKt.formatMessage("Kotlin")); //코틀린으로 출력//
+
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/kotlin/Hello.kt
@@ -0,0 +1,6 @@
+/**
+ * Created by apple on 2016. 12. 23..
+ */
+package com.androidhuman.example.hellokotlin
+
+fun formatMessage(name:String):String = "Hello, $name"; //파라미터로 name값을 입력받아 출력/
